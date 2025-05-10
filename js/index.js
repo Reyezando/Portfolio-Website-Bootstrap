4b310fabc68e67c7d370e0c116d7a0d1d180f207
Reyezando <reynardo.etantyo2@gmail.com>
2025-05-10 23:06:33 +0700

    update malam

--- js/index.js
+++ js/index.js
@@ -13,6 +13,24 @@ var splide = new Splide( '.splide', {
   
 splide.mount();
 
+// SCROLL TO TOP FUNCTION
+// Get the button
+let mybutton = document.getElementById("topButton");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 // JQUERY
 $(document).ready(function(){
     $('.infoText').hide();
